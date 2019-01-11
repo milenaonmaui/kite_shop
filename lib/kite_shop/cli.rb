f03e539bb8c33dc78b206d735c137ddd276a1e97
milenaonmaui <milenakari@yahoo.com>
2019-01-11 14:14:40 -0800

    Basic user CLi input

--- lib/kite_shop/cli.rb
+++ lib/kite_shop/cli.rb
@@ -1,5 +1,13 @@
 class KiteShop::CLI 
     def start 
-      puts "Hello World!"
+      base_url = "https://www.realwatersports.com/collections/kites/"
+ 
+      puts "Select a kite size you are interested in:"
+      puts "Enter 3, 5, 9, or 11"
+      size = gets.strip
+      url = base_url + "size_"+size+"m"
+      #puts url
     end
+
+
 end
--- lib/kite_shop/scraper.rb
+++ lib/kite_shop/scraper.rb
@@ -1,15 +1,15 @@
 class KiteShop::Scraper 
     
-    def get_page
-        Nokogiri::HTML(open("https://www.realwatersports.com/collections/kites/size_9m"))
+    def get_page(url)
+        Nokogiri::HTML(open(url))
     end
     
-      def scrape_products
+      def scrape_products(url)
         #returns an array of products
         #name - arr[0].css(".productitem--title a").text.strip
         #price - arr[0].css(".price--main").text.strip 
         #arr[0].css("a").attribute("href").value
-         self.get_page.css(".productgrid--item-kite .productitem--info")
+         self.get_page(url).css(".productgrid--item-kite .productitem--info")
          
       end
 end
